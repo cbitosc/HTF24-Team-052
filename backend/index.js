571a72f700208bc8d2db93a930129c250b89b2cc
morgenstern2 <164681976+morgenstern2@users.noreply.github.com>
2024-10-27 02:03:01 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`); // Server running message
 });
-const cors = require('cors');
-
-app.use(cors()); // Enable CORS
